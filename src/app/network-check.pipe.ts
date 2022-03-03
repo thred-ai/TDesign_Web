f5454821d983f0398d3f73413c396a14d22de5d7
Arta Koroushnia <artakoroushnia@Artas-MacBook-Pro.local>
2022-03-03 01:48:33 -0500

    PUSHY

--- src/app/network-check.pipe.ts
+++ src/app/network-check.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ethers } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 @Pipe({
   name: 'networkCheck'
@@ -7,7 +8,19 @@ import { ethers } from 'ethers';
 export class NetworkCheckPipe implements PipeTransform {
 
   transform(value: ethers.providers.Web3Provider | undefined): Promise<ethers.providers.Network> | undefined {
-    return value?.getNetwork();
+    return this.networkCheck(value);
+  }
+
+  async networkCheck(prov: ethers.providers.Web3Provider | undefined){
+    
+    var network = await (prov?.getNetwork())
+    if (network?.chainId == 0 && window.ethereum){
+      let provider = await detectEthereumProvider() as any
+      network = await provider.request({ method: 'eth_chainId' })
+    }
+    console.log(network)
+    console.log(network?.chainId)
+    return network!
   }
 
 }
